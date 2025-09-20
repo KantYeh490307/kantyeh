167475460b81a2fdeceedd6ca60494caf8cdf709
KantYeh490307 <a0980599537@gmail.com>
2025-09-20 13:53:17 +0800

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
-// 將 "你好 新世界" 顯示在網頁上
-document.getElementById('greeting').textContent = '你好 新世界';
+// 將訊息排成三行，置中顯示並放大字體
+const greeting = document.getElementById('greeting');
+greeting.innerHTML = `你好 新世界<br>我是 Kant Yeh<br>使用 VS 寫的第一個網頁`;
+greeting.style.textAlign = 'center';
+greeting.style.fontSize = '2em';
+greeting.style.marginTop = '20vh';
